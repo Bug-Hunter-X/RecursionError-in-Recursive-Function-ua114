71fcce8adb127afb0a3c64d3c6f5f606b2b3c9c4
BugHunter-X <askzenai@gmail.com>
2025-01-15 15:36:05 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,10 @@
+def my_function(x):
+    if x < 0:
+        return "Factorial is not defined for negative numbers"
+    elif x == 0:
+        return 1
+    else:
+        return x * my_function(x - 1)
+
+print(my_function(5)) # Output: 120
+print(my_function(-1)) # Output: Factorial is not defined for negative numbers
